907ac9c90af8c5f69238f3ca81ae1ec8a0c2c672
Ross McFarland <rwmcfa1@gmail.com>
2024-10-08 11:36:21 -0700

    Bump python_requires to 3.9

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     long_description_content_type='text/markdown',
     name='octodns-powerdns',
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     tests_require=tests_require,
     url='https://github.com/octodns/octodns-powerdns',
     version=version(),
